2680527707b0829e2c8db8a7f67512bc83491567
Jess Sortland <155406740+jesssortland@users.noreply.github.com>
2024-08-26 05:21:45 +0000

    Return Negative

---
+++ 8kyu/returnNegative.js
@@ -0,0 +1,11 @@
+//Return Negative
+//https://www.codewars.com/kata/55685cd7ad70877c23000102
+
+function makeNegative(num) {
+    if (num >= 0){
+      return -(num);
+    }
+    return num;
+  }
+  
+  console.log(makeNegative(7));
