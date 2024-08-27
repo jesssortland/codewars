f077a08a98aba2ff642c495e2168fd3dd618a461
Jess Sortland <155406740+jesssortland@users.noreply.github.com>
2024-08-27 04:24:45 +0000

    Even or Odd

---
+++ 8kyu/evenOrOdd.js
@@ -0,0 +1,10 @@
+//Even or Odd
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+      return 'Even';
+    }
+    return 'Odd';
+  }
+  console.log(evenOrOdd(2));
