b1227d0ae2dcde6b717a69de80363b1c8add555d
Jess Sortland <155406740+jesssortland@users.noreply.github.com>
2024-08-28 21:18:32 +0000

    Twice as old

---
+++ 8kyu/twiceAsOld.js
@@ -0,0 +1,8 @@
+//Twice as old
+//https://www.codewars.com/kata/5b853229cfde412a470000d0
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(dadYearsOld - (sonYearsOld*2));
+}
+
+console.log(twiceAsOld(36, 7));
